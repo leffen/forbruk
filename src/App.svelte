5608451de09561fdb2c2b6ce4f7f302ded022519
leffen <leffen@gmail.com>
2022-12-22 08:51:19 +0100

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import svelteLogo from "./assets/svelte.svg";
+  import Maaling from "./lib/Maaling.svelte";
+  import Chart from "./lib/Chart.svelte";
+</script>
+
+<main>
+  <h1>Forbruk</h1>
+
+  <div class="card tall">
+    <Maaling />
+    <Chart />
+  </div>
+</main>
+
+<style>
+  .tall {
+    font-size: 72px;
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+  }
+</style>
